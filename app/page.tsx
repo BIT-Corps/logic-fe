362d26400a48dc886ddcb6f2a0e416fa6d09e0d5
Stephen Ibe <saintkalson@gmail.com>
2023-12-07 09:13:01 +0100

    mantine config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="font-bold text-4xl">Hello World</h1>
+      <h1 className="font-bold text-4xl text-red-600">Hello World</h1>
     </main>
   );
 }
