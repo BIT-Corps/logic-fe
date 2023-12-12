b44fdb239a3184c00f30ec10227733fa69c4f82b
Stephen Ibe <saintkalson@gmail.com>
2023-12-12 16:41:43 +0100

    about us

---
+++ app/about/layout.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Footer, TopNav } from "../components";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const layout = ({ children }: Props) => {
+  return (
+    <>
+      <TopNav />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default layout;
