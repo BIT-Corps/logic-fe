3f9323d4abc6fba06aafb223ec406942b91293f4
Stephen Ibe <saintkalson@gmail.com>
2023-12-18 22:55:33 +0100

    contact us

---
+++ app/contact/layout.tsx
@@ -0,0 +1,11 @@
+import { WebLayout } from "../layouts";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const layout = ({ children }: Props) => {
+  return <WebLayout>{children}</WebLayout>;
+};
+
+export default layout;
