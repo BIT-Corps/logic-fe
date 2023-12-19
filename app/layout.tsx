26e5a62404e6a80527c2f478763d5d8c163e0882
Stephen Ibe <saintkalson@gmail.com>
2023-12-19 18:41:44 +0100

    PWA support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,24 @@
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "node_modules/react-modal-video/scss/modal-video.scss";
 import "./globals.css";
 import { theme } from "./lib";
 
 export const metadata: Metadata = {
   manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "LOGIC",
+  },
   title: "LOGIC",
   description:
     "The LOGIC Church stands as a beacon of The Love Of God In Christ Jesus here in Lagos Nigeria and to the world at large.",
+};
+
+export const viewport: Viewport = {
+  themeColor: "#C74747",
 };
 
 export default function RootLayout({
