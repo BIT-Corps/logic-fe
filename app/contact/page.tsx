07801ce3297868d5ae64d9ba158afb6dcd48022a
Stephen Ibe <saintkalson@gmail.com>
2023-12-21 10:00:44 +0100

    contact us MAP

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,17 +6,17 @@ const Contact = () => {
   return (
     <>
       <section className="contact_hero flex flex-col items-center justify-center text-center uppercase font-semibold">
-        <h2 className="text-5xl md:text-8xl">
+        <h2 className="text-4xl md:text-7xl lg:text-8xl">
           Reach us through the <br /> branch closest to you
         </h2>
-        <p className="text-sm md:text-lg lg:w-5/12">
+        <p className="w-10/12 text-sm md:text-lg lg:w-5/12">
           grow in faith, strength, knowledge and wisdom of god
         </p>
       </section>
 
       <section className="lg-section-white">
         <div className="lg-container space-y-14">
-          <ul className="locations mx-auto flex items-center justify-center gap-x-14">
+          <ul className="locations mx-auto flex flex-col md:flex-row items-center justify-center gap-y-5 md:gap-y-0 gap-x-10 lg:gap-x-14">
             {[
               { id: 0, title: "HQ, Lagos Island" },
               { id: 1, title: "Abuja" },
@@ -24,27 +24,29 @@ const Contact = () => {
               { id: 3, title: "Mainland - Lagos" },
               { id: 4, title: "London, UK" },
             ].map(({ id, title }) => (
-              <li key={id} className="font-semibold cursor-pointer">
+              <li key={id} className="font-semibold cursor-pointer lg:text-lg">
                 {title}
               </li>
             ))}
           </ul>
           <section className="flex flex-col justify-center gap-y-10">
             <Image src="/map.png" alt="map" width={1881} height={852} />
-            <div className="grid grid-cols-3 gap-x-10">
-              <div className="space-y-2 col-span-2">
-                <h4 className="text-2xl">Visit the Logic Center</h4>
-                <h5 className="text-4xl">
+            <div className="grid grid-cols-2 md:grid-cols-3 cleargap-x-10">
+              <div className="space-y-2 md:col-span-2">
+                <h4 className="md:text-lg lg:text-2xl">
+                  Visit the Logic Center
+                </h4>
+                <h5 className="text-sm md:text-xl lg:text-4xl">
                   4, Oriwu Street, Beside Petrocam <br /> Fueling Station, Lekki
                   phase 1 Lagos
                 </h5>
               </div>
               <div className="space-y-2">
-                <h4 className="text-2xl">Contact</h4>
-                <h5>
-                  <h6 className="text-4xl">info@thelogicchurch.org</h6>
-                  <h6 className="text-4xl">+(234) 809 668 2229</h6>
-                </h5>
+                <h4 className="md:text-lg lg:text-2xl">Contact</h4>
+                <div className="text-sm md:text-xl lg:text-4xl">
+                  <h6>info@thelogicchurch.org</h6>
+                  <h6>+(234) 809 668 2229</h6>
+                </div>
               </div>
             </div>
           </section>
